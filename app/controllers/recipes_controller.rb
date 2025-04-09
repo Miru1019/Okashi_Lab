35fe7af4ec414e45ac18f9fbd111274ccad65304
ShunYashima <186166120+Miru1019@users.noreply.github.com>
2025-04-09 07:19:20 +0000

    各ページ作成

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,55 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def index
+    @user = current_user
+    @recipes = Recipe.all
+    @recipe = Recipe.new
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @newrecipe = Recipe.new
+    @user = @recipe.user
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id]) 
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+
+    if @recipe.save
+      flash[:notice]="You have created recipe successfully."
+      redirect_to recipe_path(@recipe)
+    else
+      @user = current_user
+      @recipes = Recipe.all
+      render :index
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'レシピが更新されました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id]) 
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'レシピが削除されました。'
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image, :ingredients, :instructions)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @recipes = @user.recipes
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールが更新されました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image)
+  end
+end
