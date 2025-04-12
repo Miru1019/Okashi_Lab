3eefb5512c3ccc0b06a6eef497efb155b0c11981
ShunYashima <186166120+Miru1019@users.noreply.github.com>
2025-04-12 09:23:11 +0000

    投稿画面変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    about_path
   end
 end
