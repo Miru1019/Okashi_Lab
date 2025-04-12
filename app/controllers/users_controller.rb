2708afc499c9089592e201d1fbfbe8ede978366c
ShunYashima <186166120+Miru1019@users.noreply.github.com>
2025-04-12 12:37:48 +0000

    非ログイン時設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def mypage
     @user = current_user
     @recipes = @user.recipes
